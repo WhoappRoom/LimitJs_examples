0ff96053e13dba756c7bf82c0d9e7c6d97039286
Cabdi Waaxid Siciid <158821136+WhoappRoom@users.noreply.github.com>
2024-09-24 19:46:25 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-import { useAsRoot, addEvent, getId, style, styles, alertError, applyAlertStyles, alertSuccess } from 'https://limitjs.vercel.app/index.js'
-const app = useAsRoot('#app');
-app.get('/home', () => ({ html: '<h1>Home Page</h1>' }));
-app.get('/user/:id', (params) => ({
-  html: `<h1>User Profile for ID ${params.id}</h1>`
-}));
-app.post('/submit', () => ({ html: '<h1>Form Submitted</h1>' }));
-
-app.goRoute('get', '/home')
